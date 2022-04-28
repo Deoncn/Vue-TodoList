c051ed7cd0b9ca70557b1a940baf19b8ea10d476
deoncn <2210633536@qq.com>
2022-04-29 00:22:23 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 以相对路径为例 解决Nginx服务器上无法在文件夹里访问的问题
+  publicPath:'./'
 })
